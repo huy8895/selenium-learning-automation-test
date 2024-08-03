c6177b0f27f41763e204567d0d76cc2a6eeb8000
huytvdev <huy8895@gmail.com>
2024-08-03 23:59:13 +0700

    done login to outlook

---
+++ selenium-basic/src/main/java/org/localtors/BingReward.java
@@ -0,0 +1,52 @@
+package org.localtors;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BingReward {
+
+  public static void main(String[] args) {
+    WebDriver webDriver = new EdgeDriver();
+    webDriver.get("https://login.live.com/");
+    webDriver.manage()
+        .timeouts()
+        .implicitlyWait(Duration.ofSeconds(5));
+    try {
+
+      webDriver.findElement(By.xpath("//input[@id='i0116']"))
+          .sendKeys(args[0], Keys.ENTER);
+
+      // Chờ cho trường nhập password xuất hiện
+      WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+      WebElement passwordField = wait.until(
+          ExpectedConditions.visibilityOfElementLocated(By.id("i0118")));
+
+      // Nhập password và nhấn Enter
+      passwordField.sendKeys(args[1], Keys.ENTER);
+
+      // Chờ cho trang "Stay signed in?" xuất hiện bằng cách tìm văn bản
+      WebElement staySignedInTitle = wait.until(
+          ExpectedConditions.visibilityOfElementLocated(
+              By.xpath("//div[text()='Stay signed in?']")));
+
+// Kiểm tra nếu có chữ "Stay signed in?" và chọn "Yes"
+      if (staySignedInTitle != null) {
+        WebElement yesButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//button[@type='submit' and text()='Yes']")));
+        yesButton.click();
+      }
+
+      // Thêm các bước kiểm tra tiếp theo nếu cần thiết
+    } finally {
+      // Đóng trình duyệt
+      webDriver.quit();
+    }
+  }
+
+}
