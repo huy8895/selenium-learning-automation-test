fe1ea1a098419b26edf76f7880dc1e5a9c5a5a1e
huytvdev <huy8895@gmail.com>
2024-08-03 17:30:23 +0700

    done start edge, chrome using driver

--- selenium-basic/src/main/java/org/example/Main.java
+++ selenium-basic/src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+//    System.setProperty("webdriver.chrome.driver",
+//        "/Users/huytrinh/Work/udemy-resources/selenium/driver/chromedriver-mac-arm64/chromedriver");
+
+    final WebDriver driver = new EdgeDriver();
+    driver.get("https://www.bing.com");
+    System.out.println("driver.getTitle() = " + driver.getTitle());
+
+    driver.quit();
+
+    
   }
 }
